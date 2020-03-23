57f6e7ed7da09534ac506fda41a79f4c293666ad
Matheus Lemes Florentino <-global>
2020-03-23 14:10:58 -0300

    Add dependencia h2 + mapeando entity Categoria

--- src/main/java/br/com/cursospring/curso/entitys/CategoriaEntity.java
+++ src/main/java/br/com/cursospring/curso/entitys/CategoriaEntity.java
@@ -1,12 +1,19 @@
 package br.com.cursospring.curso.entitys;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class CategoriaEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
