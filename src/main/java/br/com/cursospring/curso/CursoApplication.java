c12c0ec766592a5c9e753318a80b042949a9ae83
Matheus Lemes Florentino <-global>
2020-03-23 14:41:23 -0300

    Inserindo diversos registros de uma vez

--- src/main/java/br/com/cursospring/curso/CursoApplication.java
+++ src/main/java/br/com/cursospring/curso/CursoApplication.java
@@ -1,13 +1,33 @@
 package br.com.cursospring.curso;
 
+import br.com.cursospring.curso.entities.CategoriaEntity;
+import br.com.cursospring.curso.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		CategoriaEntity cat1 = new CategoriaEntity(null, "Informática");
+		CategoriaEntity cat2 = new CategoriaEntity(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
+
 }
+
