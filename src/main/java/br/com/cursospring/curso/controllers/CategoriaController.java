89bf435d27ef3cd9666a14d9994eaa1b326dcdd8
Matheus Lemes Florentino <-global>
2020-03-23 13:50:34 -0300

    Classe categoria + mudanca controller

--- src/main/java/br/com/cursospring/curso/controllers/CategoriaController.java
+++ src/main/java/br/com/cursospring/curso/controllers/CategoriaController.java
@@ -1,16 +1,28 @@
 package br.com.cursospring.curso.controllers;
 
+import br.com.cursospring.curso.entitys.CategoriaEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listarCategorias(){
-        return "Tá funfando, campeão";
+    public List<CategoriaEntity> listarCategorias(){
+
+        CategoriaEntity c1 = new CategoriaEntity(1, "Informática");
+        CategoriaEntity c2 = new CategoriaEntity(2, "Escritório");
+
+        List<CategoriaEntity> categoriaist = new ArrayList<>();
+        categoriaist.add(c1);
+        categoriaist.add(c2);
+
+        return categoriaist;
     }
 
 }
