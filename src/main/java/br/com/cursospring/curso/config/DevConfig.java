49f4942dd77e95cae991037c3439187d9faa8b2f
Matheus Lemes Florentino <-global>
2020-03-24 16:35:47 -0300

    Usuario de desenvolvimento + conexao MYSQL

---
+++ src/main/java/br/com/cursospring/curso/config/DevConfig.java
@@ -0,0 +1,32 @@
+package br.com.cursospring.curso.config;
+
+import br.com.cursospring.curso.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if(strategy.equals("create")){
+            dbService.instantiateTestDatabase();
+        }
+
+        return true;
+    }
+
+}
