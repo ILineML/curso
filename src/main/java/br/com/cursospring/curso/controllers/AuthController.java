78d06c3fba22514a8b646ed50850e8bba393e17f
Matheus Lemes Florentino <-global>
2020-03-25 13:45:02 -0300

    Refresh token

---
+++ src/main/java/br/com/cursospring/curso/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package br.com.cursospring.curso.controllers;
+
+import br.com.cursospring.curso.security.JWTUtil;
+import br.com.cursospring.curso.security.UserSecurity;
+import br.com.cursospring.curso.services.security.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSecurity user = UserService.getCurrentUser();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
