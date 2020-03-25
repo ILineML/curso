c3ff3d263009a1344c064da4ccae5b86804d628d
Matheus Lemes Florentino <-global>
2020-03-25 16:33:38 -0300

    Endpoint para enviar imagem

--- src/main/java/br/com/cursospring/curso/controllers/ClienteController.java
+++ src/main/java/br/com/cursospring/curso/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -76,6 +77,15 @@ public class ClienteController {
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
                 buildAndExpand(categoria.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/imagem", method = RequestMethod.POST)
+    public ResponseEntity<Void> adicionarCliente(@RequestBody MultipartFile file){
+//        A PARTIR DAQUI, DEVE SER CRIADA A SERVICE QUE IRA SALVAR ESTA IMAGEM NA NUVEM
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(file.getName()).toUri();
 
         return ResponseEntity.created(uri).build();
     }
